71450582035ea1424246d2d043ca779b5998451e
salmalailia <73702347+salmalailia@users.noreply.github.com>
2021-04-17 11:59:32 +0700

    Add files via upload

---
+++ Soal_1/soal1.c
@@ -0,0 +1,39 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main () {
+	pid_t child = fork ();
+	int status;  
+	if (child < 0) {
+		exit(EXIT_FAILURE);
+ 	}
+    
+	if (child == 0) {
+	        pid_t child = fork();
+        
+        if (child < 0) {
+		exit(EXIT_FAILURE);
+	} 
+    }
+    
+	if (child == 0) { 
+    		char *argv[] = {"mkdir", "-p", "/home/salma/Modul 2/Pyoto", NULL};
+        	execv("/bin/mkdir", argv); }
+        
+	else if (child <= 10) {
+        	while ((wait(&status)) <= 10);
+        	char *argv[] = {"mkdir", "-p", "/home/salma/Modul 2/Musyik", NULL};
+        	execv ("/bin/mkdir", argv); }
+        	
+	else if (child >= 10) { 
+		while ((wait(&status)) >= 10);
+        	char *argv[] = {"mkdir", "-p", "/home/salma/Modul 2/Fylm", NULL};
+        	execv ("/bin/mkdir", argv);
+        }
+        
+    }
