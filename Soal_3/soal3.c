eeb7dec3cbf356ae6d7adde2191ea152304770cb
yogarsdna <74660281+yogarsdna@users.noreply.github.com>
2021-04-17 15:53:15 +0700

    Create soal3.c

---
+++ Soal_3/soal3.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+//3A - Make directory according to the format name
+void directory()
+{
+  pid_t child_id;
+  int status;
+  char datedirectory[100];
+  struct tm *timenow;
+  
+  //Save child_id from child process
+  child_id = fork();
+
+  //Make format name for the directory
+  time_t t = time(NULL);
+  timenow = localtime(&t);
+  strftime(datedirectory, sizeof(datedirectory), "%Y-%m-%d_%H:%M:%S", timenow);
+
+  if(child_id < 0) 
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  if(child_id == 0) 
+  {
+    char *argv[] = {"mkdir", date, NULL};
+    execv("/bin/mkdir", argv);
+  }
+}
+
+//Main function
+int main(int argc, char *argv[])
+{
+  directory();
+}
