fa21c197816a9cf505716d7c0fb0bc8102501c91
Zulfiqar Rahman Aji <68783399+p3trichorr@users.noreply.github.com>
2021-04-25 17:47:59 +0700

    Update soal2.c

--- Soal_2/soal2.c
+++ Soal_2/soal2.c
@@ -13,8 +13,8 @@
 #include<dirent.h>
 
 int main() {
-    int state_1, state_2, state_3, state_4;
-      
+    int state_1, state_2, state_3;
+    
     pid_t cid;
     cid = fork();
     if(cid < 0) exit(0);
@@ -23,18 +23,18 @@ int main() {
         execv("/bin/unzip", command);
     }
     
-    while(wait(&state_2) > 0);
-    DIR *dirp2;
-    struct dirent *entry2;
-    dirp2 = opendir("/home/zulu/modul2/petshop");
+    while(wait(&state_1) > 0);
+    DIR *pdr1;
+    struct dirent *entry1;
+    pdr1 = opendir("/home/zulu/modul2/petshop");
     char foldername[100][300];
-    int idx = 0;
-    while((entry2 = readdir(dirp2)) != NULL) {
-        if(entry2->d_type == DT_REG) {
+    int n = 0;
+    while((entry1 = readdir(pdr2)) != NULL) {
+        if(entry1->d_type == DT_REG) {
             char tmp[300], tmp2[300];
-            memset(foldername[idx], 0, sizeof(foldername[idx]));
+            memset(foldername[n], 0, sizeof(foldername[n]));
             memset(tmp2, 0, sizeof(tmp2));
-            strcpy(tmp, entry2->d_name);
+            strcpy(tmp, entry1->d_name);
             
             int i, found = 0;
             for(i = 0; i < strlen(tmp); i++) {
@@ -42,19 +42,19 @@ int main() {
                 tmp2[i] = tmp[i];
             }
             
-            for(i = 0; i < idx && found == 0; i++)
+            for(i = 0; i < n && found == 0; i++)
                 if(strcmp(foldername[i], tmp2) == 0)
                     found = 1;
             
             if(found == 0) {
-                strcpy(foldername[idx], tmp2);
-                idx++;
+                strcpy(foldername[n], tmp2);
+                n++;
             }
         }
     }
     
     int i;
-    for(i = 0; i < idx; i++) {
+    for(i = 0; i < n; i++) {
         pid_t cid;
         cid = fork();
         if(cid < 0) exit(0);
@@ -66,12 +66,12 @@ int main() {
         }
     }
     
-    while(wait(&state_3) > 0);
-    DIR *dirp3;
-    struct dirent *entry3;
-    dirp3 = opendir("/home/zulu/modul2/petshop");
-    while((entry3 = readdir(dirp3)) != NULL) {
-        if(entry3->d_type == DT_REG) {
+    while(wait(&state_2) > 0);
+    DIR *pdr2;
+    struct dirent *entry2;
+    pdr3 = opendir("/home/zulu/modul2/petshop");
+    while((entry2 = readdir(pdr2)) != NULL) {
+        if(entry2->d_type == DT_REG) {
             char tmp[300];
             char tmp2_jenis[300], tmp2_nama[300], tmp2_umur[300];
             char tmp3_jenis[300], tmp3_nama[300], tmp3_umur[300];
@@ -83,41 +83,41 @@ int main() {
             memset(tmp3_umur , 0, sizeof(tmp3_umur));
             strcpy(tmp, entry3->d_name);
             
-            int i, ii, found = 0, counter = 0;
+            int i, l, found = 0, counter = 0;
             for(i = 0; i < strlen(tmp); i++) {
                 if(tmp[i] == ';') break;
                 tmp2_jenis[i] = tmp[i];
             }
             
-            i++; ii = i;
+            i++; l = i;
             for(; i < strlen(tmp); i++) {
                 if(tmp[i] == ';') break;
-                tmp2_nama[i-ii] = tmp[i];
+                tmp2_nama[i-l] = tmp[i];
             }
             
-            i++; ii = i;
+            i++; l = i;
             for(; i < strlen(tmp); i++) {
                 if(tmp[i] == '_' || (tmp[i] == '.' && tmp[i+1] == 'j')) break;
-                tmp2_umur[i-ii] = tmp[i];
+                tmp2_umur[i-l] = tmp[i];
             }
             
             if(tmp[i] == '_') {
-                i++; ii = i;
+                i++; l = i;
                 for(; i < strlen(tmp); i++) {
                     if(tmp[i] == ';') break;
-                    tmp3_jenis[i-ii] = tmp[i];
+                    tmp3_jenis[i-l] = tmp[i];
                 }
                 
-                i++; ii = i;
+                i++; l = i;
                 for(; i < strlen(tmp); i++) {
                     if(tmp[i] == ';') break;
-                    tmp3_nama[i-ii] = tmp[i];
+                    tmp3_nama[i-l] = tmp[i];
                 }
                 
-                i++; ii = i;
+                i++; l = i;
                 for(; i < strlen(tmp); i++) {
                     if(tmp[i] == '.' && tmp[i+1] == 'j') break;
-                    tmp3_umur[i-ii] = tmp[i];
+                    tmp3_umur[i-l] = tmp[i];
                 }
                 
                 counter = 1;
@@ -129,13 +129,12 @@ int main() {
             if(cid < 0) exit(0);
             if(cid == 0) {
                 char asal[300], tujuan[300];
-                sprintf(asal, "/home/zulu/modul2/petshop/%s", entry3->d_name);
+                sprintf(asal, "/home/zulu/modul2/petshop/%s", entry2->d_name);
                 sprintf(tujuan, "/home/zulu/modul2/petshop/%s/%s", tmp2_jenis, tmp2_nama);
                 char *command[] = {"cp", "-r", asal, tujuan, NULL};
                 execv("/bin/cp", command);
             }
-            
-            
+          
             while(wait(&state_1) > 0);
             char ket[300];
             sprintf(ket, "/home/zulu/modul2/petshop/%s/keterangan.txt", tmp2_jenis);
@@ -144,15 +143,14 @@ int main() {
             fprintf(filep, "nama : %s\n", tmp2_nama);
             fprintf(filep, "umur : %s tahun\n\n", tmp2_umur);
             fclose(filep);
-            
-
+           
             if(counter == 1) {
                 pid_t cid;
                 cid = fork();
                 if(cid < 0) exit(0);
                 if(cid == 0) {
                     char asal[300], tujuan[300];
-                    sprintf(asal, "/home/zulu/modul2/petshop/%s", entry3->d_name);
+                    sprintf(asal, "/home/zulu/modul2/petshop/%s", entry2->d_name);
                     sprintf(tujuan, "/home/zulu/modul2/petshop/%s/%s", tmp3_jenis, tmp3_nama);
                     char *command[] = {"cp", "-r", asal, tujuan, NULL};
                     execv("/bin/cp", command);
@@ -170,18 +168,18 @@ int main() {
         }
     }
     
-    while(wait(&state_4) > 0);
-    DIR *dirp4;
-    struct dirent *entry4;
-    dirp4 = opendir("/home/zulu/modul2/petshop");
-    while((entry4 = readdir(dirp4)) != NULL) {
-        if((entry4->d_type == DT_REG) && strcmp(entry4->d_name, ".") != 0 && strcmp(entry4->d_name, "..") != 0){
+    while(wait(&state_3) > 0);
+    DIR *pdr3;
+    struct dirent *entry3;
+    pdr3 = opendir("/home/zulu/modul2/petshop");
+    while((entry3 = readdir(pdr3)) != NULL) {
+        if((entry3->d_type == DT_REG) && strcmp(entry3->d_name, ".") != 0 && strcmp(entry3->d_name, "..") != 0){
             pid_t cid;
             cid = fork();
             if(cid < 0) exit(0);
             if(cid == 0) {
                 char foldername[300];
-                sprintf(foldername, "/home/zulu/modul2/petshop/%s", entry4->d_name);
+                sprintf(foldername, "/home/zulu/modul2/petshop/%s", entry3->d_name);
                 char *command[] = {"rm", "-r", foldername, NULL};
                 execv("/bin/rm", command);
             }
